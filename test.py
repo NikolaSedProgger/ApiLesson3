00be72865505b01f265e805e066968095206eefe
NikolaSedProgger <65446783+NikolaSedProgger@users.noreply.github.com>
2020-08-22 14:03:55 +0300

    delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-
-parser = argparse.ArgumentParser(
-    description='Описание как работать с программой'
-)
-parser.add_argument('-l','--link', help='Ссылка / Битлинк')
-args = parser.parse_args()
-print(args.link)
